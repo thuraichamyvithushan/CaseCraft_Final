d9530e4a57df5db29297e27158ffbf9ab6b23c9f
thuraichamyvithushan <145326510+thuraichamyvithushan@users.noreply.github.com>
2026-01-23 17:03:34 +0530

    5

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const CLIENT_URL = process.env.CLIENT_URL
     "http://localhost:3001",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
-    "http://127.0.0.1:3001",
+    "http://127.0.0.1:3000",
     "http://127.0.0.1:5173"
   ];
 
